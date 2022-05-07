19883e277850e63b8a998a2d783ed653e21ae60d
avneetdogra <avneetdogra3992@gmail.com>
2022-05-07 18:15:11 +0530

    assignment

--- block-BJaajc/code/index.js
+++ block-BJaajc/code/index.js
@@ -1,27 +1,28 @@
+
 function getFullName(firstName, LastName) {
-    return `${firstName} ${LastName}`;
+  return `${firstName} ${LastName}`;
+}
+
+function palindrome(word) {
+  let Word = word.split('').reverse().join('');
+  if (Word === word) {
+    return true;
+  } else {
+    return false;
   }
-  
-  function palindrome(word) {
-    let Word = word.split('').reverse().join('');
-    if (Word === word) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-  
-  function getCircumfrence() {
-    return `The Circumference is ${2 * Math.PI * radius}`;
-  }
-  
-  function getArea(radius) {
-    return `The area i  s ${Math * radius * radius}`;
-  }
-  
-  module.exports = {
-    getFullName,
-    palindrome,
-    getCircumfrence,
-    getArea,
-  };
+}
+
+function getCircumfrence() {
+  return `The Circumference is ${2 * Math.PI * radius}`;
+}
+
+function getArea(radius) {
+  return `The area i  s ${Math * radius * radius}`;
+}
+
+module.exports = {
+  getFullName,
+  palindrome,
+  getCircumfrence,
+  getArea,
+};
